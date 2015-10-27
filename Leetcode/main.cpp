846e5c5effc0a54e3a3650d7762615e4b40d53c1
luckywqf <luckywqf@sina.com>
2015-10-27 14:10:03 +0800

    删除无用代码

--- Leetcode/main.cpp
+++ Leetcode/main.cpp
@@ -2,6 +2,16 @@
 
 int main() {
 	Solution s;
-	cout << s.intToRoman(6);
+	assert(s.isMatch("aa", "a") == false);
+	assert(s.isMatch("aa", "aa") == true);
+	assert(s.isMatch("aaa", "aa") == false);
+	assert(s.isMatch("aa", "a*") == true);
+	assert(s.isMatch("aa", ".*") == true);
+	assert(s.isMatch("ab", ".*") == true);
+	assert(s.isMatch("aab", "c*a*b") == true);
+	assert(s.isMatch("ab", ".*c") == false);
+	assert(s.isMatch("aaa", "a.a") == true);
+	assert(s.isMatch("aaa", "a*a") == true);
+	assert(s.isMatch("aaa", "ab*a*c*a") == true);
 	return 0;
 }
