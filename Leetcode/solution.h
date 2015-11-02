89008361ae2423c5f9e3dd14c03c9a6bb4d4bcf9
luckywqf <luckywqf@sina.com>
2015-11-03 00:30:34 +0800

    64

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -1644,6 +1644,32 @@ public:
 		return paths[m - 1][n - 1];
 	}
 
+
+	//-------------------------------------------------
+	// 64. https://leetcode.com/problems/minimum-path-sum/
+	//-------------------------------------------------
+	int minPathSum(vector<vector<int>>& grid) {
+		if (grid.empty()) {
+			return 0;
+		}
+		int m = grid.size();
+		int n = grid[0].size();
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (i == 0 && j != 0) {
+					grid[i][j] += grid[i][j - 1];
+				}
+				else if (j == 0 && i != 0) {
+					grid[i][j] += grid[i - 1][j];
+				}
+				else if (i != 0 && j != 0) {
+					grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+				}
+			}
+		}
+		return grid[m - 1][n - 1];
+	}
+
 	//-------------------------------------------------
 	// 66. https://leetcode.com/problems/plus-one/
 	//-------------------------------------------------
