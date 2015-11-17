6f87d0dbc20a60fe812fd8f193bf1c69e12e2966
luckywqf <luckywqf@sina.com>
2015-11-17 22:34:24 +0800

    162

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -2947,6 +2947,25 @@ public:
 	}
 
 	//-------------------------------------------------
+	// 162. https://leetcode.com/problems/find-peak-element/
+	//-------------------------------------------------
+	int findPeakElement(vector<int> &num) {
+		int low = 0;
+		int high = num.size() - 1;
+
+		while (low < high) {
+			int mid1 = (low + high) / 2;
+			int mid2 = mid1 + 1;
+			if (num[mid1] < num[mid2]) {
+				low = mid2;
+			} else {
+				high = mid1;
+			}
+		}
+		return low;
+	}
+
+	//-------------------------------------------------
 	// 165. https://leetcode.com/problems/compare-version-numbers/
 	//-------------------------------------------------
 	int compareVersion(string version1, string version2) {
