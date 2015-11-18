098d485913b9afa70d6f2fe4881d5a9c60dcb72d
luckywqf <luckywqf@sina.com>
2015-11-18 10:52:01 +0800

    90

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -2132,6 +2132,29 @@ public:
 
 
 	//-------------------------------------------------
+	// 90. https://leetcode.com/problems/subsets-ii/
+	//-------------------------------------------------
+	vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+		vector<vector<int>> totalset = { {} };
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < nums.size();) {
+			int count = 0; // num of elements are the same
+			while (count + i < nums.size() && nums[count + i] == nums[i])  
+				count++;
+			int previousN = totalset.size();
+			for (int k = 0; k < previousN; k++) {
+				vector<int> instance = totalset[k];
+				for (int j = 0; j < count; j++) {
+					instance.push_back(nums[i]);
+					totalset.push_back(instance);
+				}
+			}
+			i += count;
+		}
+		return totalset;
+	}
+
+	//-------------------------------------------------
 	// 91. https://leetcode.com/problems/decode-ways/
 	//-------------------------------------------------
 	int numDecodings(string s) {
