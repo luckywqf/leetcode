e77a92d3013f9359e6aec53488ad4ca7381f1c6c
luckywqf <luckywqf@sina.com>
2015-12-08 23:14:05 +0800

    139

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -2990,6 +2990,32 @@ public:
 
 
 	//-------------------------------------------------
+	// 139. https://leetcode.com/problems/word-break/
+	//-------------------------------------------------
+	bool wordBreak(string s, unordered_set<string> &dict) {
+		if (dict.empty()) {
+			return false;
+		}
+
+		vector<bool> dp(s.size() + 1, false);
+		dp[0] = true;
+
+		for (int i = 1; i <= s.size(); ++i) {
+			for (int j = i - 1; j >= 0; --j) {
+				if (dp[j]) {
+					string word = s.substr(j, i - j);
+					if (dict.find(word) != dict.end()) {
+						dp[i] = true;
+						break; 
+					}
+				}
+			}
+		}
+
+		return dp[s.size()];
+	}
+
+	//-------------------------------------------------
 	// 141. https://leetcode.com/problems/linked-list-cycle/
 	//-------------------------------------------------
 	bool hasCycle(ListNode *head) {
