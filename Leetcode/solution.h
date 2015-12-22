d41db910a5e793610d6a5588f509bb1c55cf4db9
luckywqf <luckywqf@sina.com>
2015-12-22 15:28:02 +0800

    273

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -4354,6 +4354,39 @@ public:
 		return guess - sum;
 	}
 
+
+	//-------------------------------------------------
+	// 273. https://leetcode.com/problems/integer-to-english-words/
+	//-------------------------------------------------
+	string numberToWords(int n) {
+		if (n == 0) {
+			return "Zero";
+		} else {
+			return int_string(n).substr(1);
+		}
+	}
+	string int_string(int n) {
+		const char * const below_20[] = { "One", "Two", "Three", "Four","Five","Six","Seven","Eight","Nine","Ten", 
+			"Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen" };
+		const char * const below_100[] = { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+		if (n >= 1000000000) {
+			return int_string(n / 1000000000) + " Billion" + int_string(n - 1000000000 * (n / 1000000000));
+		} else if (n >= 1000000) {
+			return int_string(n / 1000000) + " Million" + int_string(n - 1000000 * (n / 1000000));
+		} else if (n >= 1000) {
+			return int_string(n / 1000) + " Thousand" + int_string(n - 1000 * (n / 1000));
+		} else if (n >= 100) {
+			return int_string(n / 100) + " Hundred" + int_string(n - 100 * (n / 100));
+		} else if (n >= 20) {
+			return string(" ") + below_100[n / 10 - 2] + int_string(n - 10 * (n / 10));
+		} else if (n >= 1) {
+			return string(" ") + below_20[n - 1];
+		} else {
+			return "";
+		}
+	}
+
+
 	//-------------------------------------------------
 	// 278. https://leetcode.com/problems/add-digits/
 	//-------------------------------------------------
