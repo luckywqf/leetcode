f9bbd7bb0aa8aad34d9aea5f01e058db9ba75464
luckywqf <luckywqf@sina.com>
2015-12-22 14:10:08 +0800

    221 222

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -3894,6 +3894,60 @@ public:
 		return false;
 	}
 
+
+	//-------------------------------------------------
+	// 221. https://leetcode.com/problems/maximal-square/
+	//-------------------------------------------------
+	int maximalSquare(vector<vector<char>>& matrix) {
+		int m = matrix.size();
+		if (m == 0) {
+			return 0;
+		}
+		int n = matrix[0].size();
+		vector<vector<int>> size(m, vector<int>(n, 0));
+		int maxsize = 0;
+		for (int j = 0; j < n; j++) {
+			size[0][j] = matrix[0][j] - '0';
+			maxsize = max(maxsize, size[0][j]);
+		}
+		for (int i = 1; i < m; i++) {
+			size[i][0] = matrix[i][0] - '0';
+			maxsize = max(maxsize, size[i][0]);
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				if (matrix[i][j] == '1') {
+					size[i][j] = min(size[i - 1][j - 1], min(size[i - 1][j], size[i][j - 1])) + 1;
+					maxsize = max(maxsize, size[i][j]);
+				}
+			}
+		}
+		return maxsize * maxsize;
+	}
+
+
+	//-------------------------------------------------
+	// 222. https://leetcode.com/problems/count-complete-tree-nodes/
+	//-------------------------------------------------
+	int countNodes(TreeNode* root) {
+		if (!root) {
+			return 0;
+		}
+		int hl = 0, hr = 0;
+		TreeNode *l = root, *r = root;
+		while (l) { 
+			hl++; l = l->left; 
+		}
+		while (r) { 
+			hr++; r = r->right; 
+		}
+		if (hl == hr) {
+			return pow(2, hl) - 1;
+		}
+		return 1 + countNodes(root->left) + countNodes(root->right);
+	}
+
+
 	//-------------------------------------------------
 	// 223. https://leetcode.com/problems/rectangle-area/
 	//-------------------------------------------------
