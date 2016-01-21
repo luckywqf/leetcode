79a78e91f948c54e88596eb6dfc834db3a69febd
luckywqf <luckywqf@sina.com>
2016-01-21 11:42:44 +0800

    41

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -1343,6 +1343,25 @@ public:
 	}
 
 	//-------------------------------------------------
+	// 41. https://leetcode.com/problems/first-missing-positive/
+	//-------------------------------------------------
+	int firstMissingPositive(vector<int>& nums) {
+		for (int i = 0; i < nums.size(); ++i) {
+			while (nums[i] > 0 && nums[i] <= nums.size() && nums[nums[i] - 1] != nums[i]) {
+				swap(nums[i], nums[nums[i] - 1]);
+			}
+		}
+
+		for (int i = 0; i < nums.size(); ++i) {
+			if (nums[i] != i + 1) {
+				return i + 1;
+			}
+		}
+
+		return nums.size() + 1;
+	}
+
+	//-------------------------------------------------
 	// 43. https://leetcode.com/problems/multiply-strings/
 	//-------------------------------------------------
 	const unsigned char ANSII_CHAR_0 = '0';
