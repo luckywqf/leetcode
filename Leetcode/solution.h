a4badbc2067385fba651cad9fd9e16f3515c8725
luckywqf <luckywqf@sina.com>
2016-02-17 13:20:15 +0800

    264

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -4729,6 +4729,22 @@ public:
 	}
 
 	//-------------------------------------------------
+	// 264. https://leetcode.com/problems/ugly-number-ii/
+	//-------------------------------------------------
+	int nthUglyNumber(int n) {
+		int t2 = 0, t3 = 0, t5 = 0; //pointers for 2, 3, 5
+		vector<int> k(n, 1);
+		for (int i = 1; i < n; i++)	{
+			k[i] = min(k[t2] * 2, min(k[t3] * 3, k[t5] * 5));
+			if (k[i] == k[t2] * 2) t2++;
+			if (k[i] == k[t3] * 3) t3++;
+			if (k[i] == k[t5] * 5) t5++;
+		}
+		return k[n - 1];
+	}
+
+
+	//-------------------------------------------------
 	// 268. https://leetcode.com/problems/missing-number/
 	//-------------------------------------------------
 	int missingNumber(vector<int>& nums) {
