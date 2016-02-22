983aaa3881547b9c922c5e4da495417bcd4c093d
luckywqf <luckywqf@sina.com>
2016-02-22 20:45:03 +0800

    334

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -4410,6 +4410,13 @@ public:
 	}
 
 	//-------------------------------------------------
+	// 227. https://leetcode.com/problems/basic-calculator-ii/
+	//-------------------------------------------------
+	int calculateii(string s) {
+
+	}
+
+	//-------------------------------------------------
 	// 228. https://leetcode.com/problems/summary-ranges/
 	//-------------------------------------------------
 	vector<string> summaryRanges(vector<int>& nums) {
@@ -5055,6 +5062,25 @@ public:
 		}
 		return result;
 	}
+
+
+	//-------------------------------------------------
+	// 334. https://leetcode.com/problems/remove-duplicate-letters/
+	//-------------------------------------------------
+	bool increasingTriplet(vector<int>& nums) {
+		int c1 = INT_MAX, c2 = INT_MAX;
+		for (int x : nums) {
+			if (x <= c1) {
+				c1 = x;           // c1 is min seen so far (it's a candidate for 1st element)
+			} else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
+				c2 = x;           // x is better than the current c2, store it
+			} else {              // here when we have/had c1 < c2 already and x > c2
+				return true;      // the increasing subsequence of 3 elements exists
+			}
+		}
+		return false;
+	}
+
 };
 
 //-------------------------------------------------
