e076a82ba22e8f8596c4079b5953945bc007c8a3
luckywqf <luckywqf@sina.com>
2016-03-10 11:23:03 +0800

    207 210

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -4504,6 +4504,36 @@ public:
 
 		return last;
 	}
+
+	//-------------------------------------------------
+	// 207. https://leetcode.com/problems/course-schedule/
+	//-------------------------------------------------
+	bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+		vector<unordered_set<int>> graph(numCourses);
+		for (auto pre : prerequisites) {
+			graph[pre.second].insert(pre.first);
+		}
+
+		vector<int> degrees(graph.size(), 0);
+		for (auto neighbors : graph) {
+			for (int neigh : neighbors) {
+				degrees[neigh]++;
+			}
+		}
+
+		for (int i = 0; i < numCourses; i++) {
+			int j = 0;
+			for (; j < numCourses; j++)
+				if (!degrees[j]) break;
+			if (j == numCourses) return false;
+			degrees[j] = -1;
+			for (int neigh : graph[j])
+				degrees[neigh]--;
+		}
+		return true;
+	}
+
+
 	//-------------------------------------------------
 	// 209. https://leetcode.com/problems/minimum-size-subarray-sum/
 	//-------------------------------------------------
@@ -4526,6 +4556,32 @@ public:
 		return min == INT_MAX ? 0 : min;
 	}
 
+	//-------------------------------------------------
+	// 210. https://leetcode.com/problems/course-schedule-ii/
+	//-------------------------------------------------
+	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+		vector<unordered_set<int>> graph(numCourses);
+		for (auto pre : prerequisites) {
+			graph[pre.second].insert(pre.first);
+		}
+		vector<int> toposort;
+		vector<bool> onpath(numCourses, false), visited(numCourses, false);
+		for (int i = 0; i < numCourses; i++) {
+			if (!visited[i] && dfs(graph, i, onpath, visited, toposort))
+				return{};
+		}
+		std::reverse(toposort.begin(), toposort.end());
+		return toposort;
+	}
+	bool dfs(vector<unordered_set<int>>& graph, int node, vector<bool>& onpath, vector<bool>& visited, vector<int>& toposort) {
+		if (visited[node]) return false;
+		onpath[node] = visited[node] = true;
+		for (int neigh : graph[node])
+			if (onpath[neigh] || dfs(graph, neigh, onpath, visited, toposort))
+				return true;
+		toposort.push_back(node);
+		return onpath[node] = false;
+	}
 
 	//-------------------------------------------------
 	// 213. https://leetcode.com/problems/house-robber-ii/
