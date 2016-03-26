f804ce556eebc56b06ffd7b64885f2620b8f02da
luckywqf <luckywqf@sina.com>
2016-03-26 15:07:16 +0800

    300 306

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -4847,6 +4847,13 @@ public:
 		return false;
 	}
 
+	//-------------------------------------------------
+	// 220. https://leetcode.com/problems/contains-duplicate-iii/
+	//-------------------------------------------------
+	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+
+	}
+
 
 	//-------------------------------------------------
 	// 221. https://leetcode.com/problems/maximal-square/
@@ -5768,12 +5775,65 @@ public:
 	}
 
 	//-------------------------------------------------
-	// 300. https://leetcode.com/problems/contains-duplicate-iii/
+	// 300. https://leetcode.com/problems/longest-increasing-subsequence/
 	//-------------------------------------------------
-	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-
+	int lengthOfLIS(vector<int>& nums) {
+		vector<int> res;
+		for (int i = 0; i < nums.size(); i++) {
+			auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+			if (it == res.end()) {
+				res.push_back(nums[i]);
+			}
+			else {
+				*it = nums[i];
+			}
+		}
+		return res.size();
 	}
-
+	//-------------------------------------------------
+	// 306. https://leetcode.com/problems/additive-number/
+	//-------------------------------------------------
+	bool isAdditiveNumber(string num) {
+		for (int i = 1; i <= num.size() / 2; i++) {
+			for (int j = 1; j <= (num.size() - i) / 2; j++) {
+				if (check(num.substr(0, i), num.substr(i, j), num.substr(i + j))) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	bool check(string num1, string num2, string num) {
+		if (num1.size() > 1 && num1[0] == '0'
+			|| num2.size() > 1 && num2[0] == '0') {
+			return false;
+		}
+		string sum = add(num1, num2);
+		if (num == sum) {
+			return true;
+		}
+		if (num.size() <= sum.size() ||
+			sum.compare(num.substr(0, sum.size())) != 0) {
+			return false;
+		}
+		else {
+			return check(num2, sum, num.substr(sum.size()));
+		}
+	}
+	string add(string n, string m) {
+		string res;
+		int i = n.size() - 1, j = m.size() - 1, carry = 0;
+		while (i >= 0 || j >= 0) {
+			int sum = carry + (i >= 0 ? (n[i--] - '0') : 0) + (j >= 0 ? (m[j--] - '0') : 0);
+			res.push_back(sum % 10 + '0');
+			carry = sum / 10;
+		}
+		if (carry) {
+			res.push_back(carry + '0');
+		}
+		std::reverse(res.begin(), res.end());
+		return res;
+	}
 
 	//-------------------------------------------------
 	// 315. https://leetcode.com/problems/remove-duplicate-letters/
@@ -6439,4 +6499,39 @@ public:
 
 private:
 	vector<vector<int>> _matrix;
+};
+
+//-------------------------------------------------
+// 307. https://leetcode.com/problems/range-sum-query-mutable/
+//-------------------------------------------------
+class NumArrayii {
+public:
+	NumArrayii(vector<int> &nums) {
+		if (nums.empty()) {
+			return;
+		}
+		data = nums;
+		sum.resize(nums.size());
+		sum[0] = nums[0];
+		for (int i = 1; i < nums.size(); i++) {
+			sum[i] = sum[i - 1] + nums[i];
+		}
+	}
+
+	void update(int i, int val) {
+		int diff = val - data[i];
+		data[i] = val;
+		for (int j = i; j < sum.size(); j++) {
+			sum[j] += diff;
+		}
+	}
+
+	int sumRange(int i, int j) {
+		int low = (i == 0) ? 0 : sum[i - 1];
+		return sum[j] - low;
+	}
+
+private:
+	vector<int> sum;
+	vector<int> data;
 };
