a3070ac1444aba957f14217077a9f0de92cf8b5b
luckywqf <luckywqf@sina.com>
2016-03-29 09:36:18 +0800

    335

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -5918,6 +5918,38 @@ public:
 		return false;
 	}
 
+	//-------------------------------------------------
+	// 335. https://leetcode.com/problems/self-crossing/
+	//-------------------------------------------------
+	bool isSelfCrossing(vector<int>& x) {
+		int len = x.size();
+		if (len <= 3) {
+			return false;
+		}
+
+		// Categorize the self-crossing scenarios, there are 3 of them: 
+		// 1. Fourth line crosses first line and works for fifth line crosses second line and so on...
+		// 2. Fifth line meets first line and works for the lines after
+		// 3. Sixth line crosses first line and works for the lines after
+		for (int i = 3; i < len; i++) {
+			if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) {
+				return true; //Fourth line crosses first line and onward
+			}
+			if (i >= 4)	{
+				if (x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) {
+					return true; // Fifth line meets first line and onward
+				}
+			}
+			if (i >= 5) {
+				if (x[i - 2] - x[i - 4] >= 0 && x[i] >= x[i - 2] - x[i - 4] &&
+					x[i - 1] >= x[i - 3] - x[i - 5] && x[i - 1] <= x[i - 3]) {
+					return true;  // Sixth line crosses first line and onward
+				}
+			}
+		}
+		return false;
+	}
+
 
 	//-------------------------------------------------
 	// 337. https://leetcode.com/problems/house-robber-iii/
