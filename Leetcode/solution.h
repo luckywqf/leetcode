7301e1855af3c5ec6892bcfdea8dcf50897e2a30
luckywqf <luckywqf@sina.com>
2016-04-04 21:18:48 +0800

    135

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -3905,6 +3905,26 @@ public:
 		return (total + tank < 0) ? -1 : start;
 	}
 
+	//-------------------------------------------------
+	// 135. https://leetcode.com/problems/candy/
+	//-------------------------------------------------
+	int candy(vector<int>& ratings) {
+		int size = ratings.size();
+		if (size <= 1)
+			return size;
+		vector<int> num(size, 1);
+		for (int i = 1; i < size; i++) {
+			if (ratings[i] > ratings[i - 1])
+				num[i] = num[i - 1] + 1;
+		}
+		for (int i = size - 1; i > 0; i--) {
+			if (ratings[i - 1] > ratings[i])
+				num[i - 1] = max(num[i] + 1, num[i - 1]);
+		}
+
+		return std::accumulate(num.begin(), num.end(), 0);
+	}
+
 
 	//-------------------------------------------------
 	// 136. https://leetcode.com/problems/single-number/
