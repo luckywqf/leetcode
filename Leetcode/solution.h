c14f1ecc4167ea23e7adf66f0e3e349a2c9e5926
luckywqf <luckywqf@sina.com>
2016-04-04 12:14:09 +0800

    322

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -5869,6 +5869,31 @@ public:
         return sqrt(n);
     }
     
+
+	//-------------------------------------------------
+	// 322. https://leetcode.com/problems/coin-change/
+	//-------------------------------------------------
+	int coinChange(vector<int>& coins, int amount) {
+		int Max = INT_MAX;
+		vector<int> dp(amount + 1, Max);
+		dp[0] = 0;
+		for (int i = 1; i <= amount; i++) {
+			for (int j = 0; j < coins.size(); j++) {
+				if (coins[j] <= i) {
+					dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+				}
+			}
+		}
+		return dp[amount] > amount ? -1 : dp[amount];
+	}
+
+	//-------------------------------------------------
+	// 324. https://leetcode.com/problems/wiggle-sort-ii/
+	//-------------------------------------------------
+	void wiggleSort(vector<int>& nums) {
+
+	}
+
 	//-------------------------------------------------
 	// 326. https://leetcode.com/problems/power-of-three/
 	//-------------------------------------------------
