af69f3e986fa91ab6609796b13e919c56dedbf61
luckywqf <luckywqf@sina.com>
2016-09-04 21:28:07 +0800

    368

--- Leetcode/solution.h
+++ Leetcode/solution.h
@@ -6657,6 +6657,40 @@ public:
 		return false;
 	}
 
+	//-------------------------------------------------
+	// 368. https://leetcode.com/problems/largest-divisible-subset/
+	//-------------------------------------------------
+	vector<int> largestDivisibleSubset(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+
+		vector<int> subset(nums.size(), 0);
+		vector<int> parent(nums.size(), 0);
+
+		int max = 0, mi = 0;
+
+		for (int i = nums.size() - 1; i >= 0; --i) { // iterate from end to start since it's easier to track the answer index
+			for (int j = i; j < nums.size(); ++j) {
+				// check every a[j] that is larger than a[i]
+				if (nums[j] % nums[i] == 0 && subset[i] < 1 + subset[j]) {
+					// if a[j] mod a[i] == 0, it means T[j] can form a larger subset by putting a[i] into T[j]
+					subset[i] = 1 + subset[j];
+					parent[i] = j;
+
+					if (subset[i] > max) {
+						max = subset[i];
+						mi = i;
+					}
+				}
+			}
+		}
+
+		vector<int> ret;
+		for (int i = 0; i < max; ++i)	{
+			ret.push_back(nums[mi]);
+			mi = parent[mi];
+		}
+		return ret;
+	}
 };
 
 //-------------------------------------------------
